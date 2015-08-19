3c2906434952b9036ec56cad7a022a73de50cfc6
manavkohli <manavk123@gmail.com>
2015-08-18 19:55:19 -0700

    added intro section removed comments

--- global.js
+++ global.js
@@ -22,88 +22,10 @@ $(document).ready(function (){
                     }, 750);
             });
 
+            $("#about-arrow").click(function (){
+                $('html, body').animate({
+                        scrollTop: $("#projects").offset().top
+                    }, 750);
+            });
+
         });
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 0) {
-    $('.theBlock').stop().animate({ right: '0px' }, 500);
-} else {
-    $('.theBlock').stop().animate({ right: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 100) {
-    $('.titleBlock').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.titleBlock').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 700) {
-    $('.whereBlock').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.whereBlock').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 900) {
-    $('.playBlock').stop().animate({ right: '0px' }, 500);
-} else {
-    $('.playBlock').stop().animate({ right: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 700) {
-    $('.movieBlock').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.movieBlock').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 700) {
-    $('.movieTwoBlock').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.movieTwoBlock').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 1000) {
-    $('.workTitle').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.workTitle').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 1200) {
-    $('.experience').stop().animate({ left: '0px' }, 500);
-} else {
-    $('.experience').stop().animate({ left: '-800px' }, 0);
-}
-});
-
-//research
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 1000) {
-    $('.research-title').stop().animate({ right: '0px' }, 500);
-} else {
-    $('.research-title').stop().animate({ right: '-800px' }, 0);
-}
-});
-
-jQuery(window).scroll(function() {
-if ($(this).scrollTop() > 1200) {
-    $('.research').stop().animate({ right: '0px' }, 500);
-} else {
-    $('.research').stop().animate({ right: '-800px' }, 0);
-}
-});
-
-
-
