eabcf3b1ce18b9d80eb036fa5afacf04aaf8f530
manavkohli <manavk123@gmail.com>
2015-08-21 17:05:32 -0700

    added project sections, fuck js scope

--- global.js
+++ global.js
@@ -1,37 +1,72 @@
+
 $(document).ready(function (){
-            //Work
-            $("#work-button-title").click(function (){
-                    $('html, body').animate({
-                        scrollTop: $("#work").offset().top
-                    }, 750);
-            });
-        	
-        	//Play
-        	$("#play-button-title").click(function (){
-                //$(this).animate(function(){
-                    $('html, body').animate({
-                        scrollTop: $("#play").offset().top
-                    }, 750);
-                //});
-            });
+    var scroll_list = [["#work-button-title", "#jobs"], ["#play-button-title", 
+                        "#jameez"], ["#about-link", "#about"], ["#about-arrow", 
+                        "#jameez"], ["#jameez-arrow", "#site"], ["#site-arrow",
+                        "#political-research"], ["#political-research-arrow",
+                        "#science-research"]];
 
-        	//About
-        	$("#about-link").click(function (){
-                    $('html, body').animate({
-                        scrollTop: $("#about").offset().top
-                    }, 750);
-            });
+    $("#work-button-title").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#jobs").offset().top
+        }, 750);
+    });
 
-            $("#about-arrow").click(function (){
-                $('html, body').animate({
-                        scrollTop: $("#jameez").offset().top
-                    }, 750);
-            });
+    $("#play-button-title").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#jameez").offset().top
+        }, 750);
+    });
 
-            $("#jameez-arrow").click(function (){
-                $('html, body').animate({
-                        scrollTop: $("#site").offset().top
-                    }, 750);
-            });
+    $("#about-link").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 750);
+    });
 
-        });
+    $("#about-arrow").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#jameez").offset().top
+        }, 750);
+    });
+
+    $("#jameez-arrow").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#site").offset().top
+        }, 750);
+    });
+
+    $("#site-arrow").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#political-research").offset().top
+        }, 750);
+    });
+
+    $("#political-research-arrow").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#science-research").offset().top
+        }, 750);
+    });
+
+    $("#project").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#science-research").offset().top
+        }, 750);
+    });
+
+    $("#science-research").click(function(){
+            $('html, body').animate({
+            scrollTop: $("#jobs").offset().top
+        }, 750);
+    });
+
+    // for(i=0; i<scroll_list.length; i++){
+    //     var source = scroll_list[i][0]
+    //     var dest = scroll_list[i][1];
+    //     $(source).click(function(){
+    //         $('html, body').animate({
+    //         scrollTop: $(dest).offset().top
+    //     }, 750);
+    //     });
+    // };
+});
